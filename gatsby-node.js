4df5f9a439950ad7af6d25d9c7939181b84888d5
Efrén Pastor <epastor@bodas.net>
2019-11-25 17:55:50 +0100

    feature: remove sourcemaps from production

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,14 @@
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  if (getConfig().mode === 'production') {
+    actions.setWebpackConfig({
+      devtool: false
+    })
+  }
+}
+
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === 'Mdx') {
